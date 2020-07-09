ea2b36848ed07bfaa495b0ba2b23119d12b35536
Lejdi <filip.starobrat@gmail.com>
2020-07-09 16:59:48 +0200

    Little code cleanup

--- TRNG.py
+++ TRNG.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 import math
 import sounddevice as sd
 
-
 def entropy(n, size):
     result = 0
     for i in n:
         if i>0:
             result += ((i / size) * math.log2(i / size))
     return -result
-
 
 recSize=100000
 mask=0b111
@@ -28,13 +26,6 @@ allBits=''
 for i in threeBits:
     allBits+=i
 
-#code for testing compression:
-"""
-newFile = open("filename1.bin", "wb")
-newFile.write(b"%r"%allBits[0:100])
-newFile.close()
-"""
-
 #generating short words from all bits
 wordSize=int(input("Enter word size:"))
 if wordSize<=0 or wordSize>recSize:
@@ -49,10 +40,10 @@ for i in range (0, int(newSize/wordSize)):
     word[i].zfill(wordSize)
     word[i]+=allBits[i*wordSize : i*wordSize+wordSize]
     word[i]=word[i][-wordSize:]
-#drawing histogram
-"""
+
+
+#drawing histogram and calculate entrophy
 numbins=2**wordSize
 n, bins, patches = plt.hist(word, numbins, facecolor='blue', alpha=1)
-plt.show()
-print("entropy: %f"%entropy(n,int(newSize/wordSize)))
-"""
+#plt.show()
+print("entrophy: %f"%entropy(n,int(newSize/wordSize)))
